3fbb6dbb46de7c59c2559b801a0ae1f0ab5602c9
gregory nowakowski <gregnb@gmail.com>
2017-05-12 11:17:25 -0400

    init build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-modules.exports = {
+module.exports = {
   watchOptions: {
     aggregateTimeout: 300,
     poll: 1000
@@ -12,7 +12,7 @@ modules.exports = {
   },
   devtool: 'eval-cheap-source-map',
   entry: {
-    app: "./src/index.js",
+    app: "./src/index.js"
   },
   output: {
     path: path.resolve(__dirname, 'dist/'),
@@ -21,21 +21,21 @@ modules.exports = {
   plugins: [
     new ExtractTextPlugin('css/build-styles.css'),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /(node_modules)/,
-          loader: 'babel-loader'
-        },
-        {
-          test: /\.css$/,
-          loader:  ExtractTextPlugin.extract({
-            loader: 'css-loader?-url&importLoaders=1&modules=true&localIdentName=[name]_[local]_[sha1:hash:hex:5]',
-          }),
-        }		
-      ]
-    }
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader:  ExtractTextPlugin.extract({
+          loader: 'css-loader?-url&importLoaders=1&modules=true&localIdentName=[name]_[local]_[sha1:hash:hex:5]',
+        }),
+      }		
+    ]
+  }
 };
 
