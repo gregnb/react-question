9cefdf91a5617a1ff83092071826cbe2eb63b552
gregn <gregnb@gmail.com>
2017-06-20 14:16:56 -0400

    gh-pages update

--- src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const ADD_QUESTION = 'ADD_QUESTION';
-export const ANSWER_QUESTION = 'ANSWER_QUESTION';
-export const DELETE_QUESTION = 'DELETE_QUESTION';
-export const GET_QUESTION = 'GET_QUESTION';
-
